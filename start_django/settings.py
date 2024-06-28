1f2a4566ebb934e92d131ab4ed4a977287673967
Muhammadjon <xamidovuhammadjon@748gmail.com>
2024-06-28 13:32:25 +0500

    templates

--- start_django/settings.py
+++ start_django/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'start_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
